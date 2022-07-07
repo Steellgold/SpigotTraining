fc74126abef48393c89476ffb4007b51da0454ec
Gaëtan H <steellgold0@gmail.com>
2022-07-07 23:55:45 +0200

    add prefixs

--- src/main/java/fr/steellgold/training/Main.java
+++ src/main/java/fr/steellgold/training/Main.java
@@ -1,1 +1,4 @@
+    public static String PREFIX = "§e§l(!) §r§e";
+    public static String PREFIX_INFO = "§3§l(!) §r§3";
+    public static String PREFIX_ERROR = "§c§l(!) §r§c";
         this.getCommand("kits").setExecutor(new KitCommand());
