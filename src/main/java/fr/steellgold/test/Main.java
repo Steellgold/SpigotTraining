e8878819a01d9a1d1adaef9039eefad36067f5a6
Gaëtan H <steellgold0@gmail.com>
2022-07-07 18:42:38 +0200

    omyguade

--- src/main/java/fr/steellgold/test/Main.java
+++ src/main/java/fr/steellgold/test/Main.java
@@ -4,7 +4,6 @@ import fr.steellgold.test.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
--- src/main/java/fr/steellgold/test/listeners/PlayerListener.java
+++ src/main/java/fr/steellgold/test/listeners/PlayerListener.java
@@ -1,5 +1,6 @@
 package fr.steellgold.test.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -9,6 +10,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.Objects;
 
@@ -28,12 +31,48 @@ public class PlayerListener implements Listener {
         ItemStack item = event.getItem();
 
         if (Objects.equals(item, new ItemStack(Material.COMPASS))) {
-            Inventory inventory = server.createInventory(null, 9, "Menu");
-            for (int i = 0; i < 9 * 3; i++) {
+            Inventory inventory = server.createInventory(null, 54, "Menu");
+            for (int i = 0; i < 54; i++) {
                 inventory.setItem(i, new ItemStack(Material.BLACK_STAINED_GLASS_PANE));
             }
 
+            for (int i = 0; i < 4; i++) {
+                for (int j = 10 + (9 * i); j < 17 + (9 * i); j++) {
+                    inventory.setItem(j, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
+                }
+            }
+
+            // 0-9
+            inventory.setItem(4, setCustomName(new ItemStack(Material.PLAYER_HEAD),ChatColor.RESET + player.getName()));
+
+            // 10-18
+            inventory.setItem(11, new ItemStack(Material.BEDROCK));
+            inventory.setItem(15, new ItemStack(Material.GOLD_INGOT));
+
+            // 19-27
+            inventory.setItem(11, new ItemStack(Material.BEDROCK));
+            inventory.setItem(15, new ItemStack(Material.GOLD_INGOT));
+
+            // 28-36
+            inventory.setItem(22, new ItemStack(Material.RED_BED));
+
+            // 37-45
+            inventory.setItem(28, new ItemStack(Material.BEDROCK));
+            inventory.setItem(34, new ItemStack(Material.WRITABLE_BOOK));
+
+            // 36-44
+            inventory.setItem(39, new ItemStack(Material.BEDROCK));
+            inventory.setItem(41, new ItemStack(Material.BEACON));
+
             player.openInventory(inventory);
         }
+    }
+
+    private ItemStack setCustomName(ItemStack item, String name) {
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+        item.setItemMeta(meta);
+        return item;
     }
 }
