59d36b1975afcd1a0e2cc0fa1e1d9d1f234487a7
Gaëtan H <steellgold0@gmail.com>
2022-07-07 23:56:03 +0200

    test

--- src/main/java/fr/steellgold/training/Main.java
+++ src/main/java/fr/steellgold/training/Main.java
@@ -1,4 +1,22 @@
+package fr.steellgold.training;
+
+import fr.steellgold.training.commands.KitCommand;
+import fr.steellgold.training.listeners.PlayerListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
     public static String PREFIX = "§e§l(!) §r§e";
     public static String PREFIX_INFO = "§3§l(!) §r§3";
     public static String PREFIX_ERROR = "§c§l(!) §r§c";
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        System.out.println(this.getConfig().get("kits"));
+
+        this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         this.getCommand("kits").setExecutor(new KitCommand());
+    }
+}
