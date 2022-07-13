7215d026509a8325158d0499aa8592bfd888594e
Gaëtan H <steellgold0@gmail.com>
2022-07-13 15:24:03 +0200

    Create PlayerListeners.java

---
+++ src/main/java/fr/steellgold/training/listeners/PlayerListeners.java
@@ -0,0 +1,18 @@
+package fr.steellgold.training.listeners;
+
+import fr.steellgold.training.Main;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListeners implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage("");
+
+        Player player = event.getPlayer();
+        player.sendMessage(Main.PREFIX + "Welcome in the server §n" + player.getName() + " §e§r!");
+    }
+}
