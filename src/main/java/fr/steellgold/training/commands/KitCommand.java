fe12f6578c7dbd2e5c89043da82b5a8f289fd959
Gaëtan H <steellgold0@gmail.com>
2022-07-13 15:23:38 +0200

    Edit the kit command

--- src/main/java/fr/steellgold/training/commands/KitCommand.java
+++ src/main/java/fr/steellgold/training/commands/KitCommand.java
@@ -1,7 +1,6 @@
 package fr.steellgold.training.commands;
 
 import fr.steellgold.training.Main;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -9,20 +8,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class KitCommand implements CommandExecutor {
+public class KitCommand implements CommandExecutor, Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-
+        if (sender instanceof Player player) {
             Inventory inventory = player.getServer().createInventory(null, 9, "Kits");
 
             for (int i = 0; i < 9; i++) {
@@ -30,10 +27,8 @@ public class KitCommand implements CommandExecutor {
             }
 
             ConfigurationSection section = Main.getInstance().getConfig().getConfigurationSection("kits");
+            assert section != null;
             for (String string : section.getKeys(false)) {
-                System.out.println(section.getString(string + ".name"));
-                System.out.println(section.getInt(string + ".price"));
-                System.out.println(section.getString(string + ".item"));
                 inventory.setItem(Integer.parseInt(string), withCustomName(
                         new ItemStack(Material.valueOf(section.getString(string + ".item"))),
                         section.getString(string + ".name"),
@@ -60,7 +55,7 @@ public class KitCommand implements CommandExecutor {
             lore.add(ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', line));
         }
         meta.setLore(lore);
-        
+
         // SET
         item.setItemMeta(meta);
         return item;
